3cd32efe31b2c3a05e0878216161f9c795c531eb
Huu Duc <hduc59111@gmail.com>
2026-07-24 00:16:03 +0700

    dang ky

--- NoteApp_project-main/Backend/app/models/user.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from app.config.database import Base
-
-class User(Base):
-    __tablename__ = "users"
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(50), unique=True)
-    email = Column(String(100), unique=True)
-    password = Column(String(255))
